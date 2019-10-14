7fcb7f3ea1096b9f4ab2f1a12758b97e7c06a24c
Itay Finci <Itay@Finci.org>
2019-10-14 22:22:32 +0300

    working on #4
    added swipe from left to right to delete item
    
    Signed-off-by: Itay Finci <Itay@Finci.org>

--- app/src/main/java/itay/finci/org/gastracker/fragments/listOfBills.java
+++ app/src/main/java/itay/finci/org/gastracker/fragments/listOfBills.java
@@ -1,11 +1,15 @@
 package itay.finci.org.gastracker.fragments;
 
 
+import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -51,10 +55,13 @@ public class listOfBills extends Fragment {
         listView.setLayoutManager(layoutManager);
 
         //add the adapter to the list
-        final StableArrayAdapter adapter = new StableArrayAdapter(BillList.getInstance().getList(), getContext());
+        final StableArrayAdapter adapter = new StableArrayAdapter(BillList.getInstance().getList(),
+                getContext(), getActivity());
         listView.setAdapter(adapter);
 
-
+        //add support for swiping items left/right
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(new SwipeManager(adapter));
+        itemTouchHelper.attachToRecyclerView(listView);
 
         return root;
 
@@ -62,6 +69,9 @@ public class listOfBills extends Fragment {
 
 }
 
+/**
+ * adapter that manages the representation of each item in the array list of bills into rows
+ */
 class StableArrayAdapter extends RecyclerView.Adapter<StableArrayAdapter.MyViewHolder> {
 
     /***
@@ -86,15 +96,17 @@ class StableArrayAdapter extends RecyclerView.Adapter<StableArrayAdapter.MyViewH
 
     private ArrayList<Bill> arrayList;
     private LayoutInflater mInflater;
+    private Activity mActivity;
 
     /**
      * constructor
      * @param objects the ArrayList of the objects
      * @param context the application context
      */
-    public StableArrayAdapter(ArrayList<Bill> objects, Context context){
+    public StableArrayAdapter(ArrayList<Bill> objects, Context context, Activity activity){
         arrayList = objects;
         this.mInflater = LayoutInflater.from(context);
+        mActivity = activity;
 
     }
 
@@ -132,4 +144,80 @@ class StableArrayAdapter extends RecyclerView.Adapter<StableArrayAdapter.MyViewH
     public int getItemCount() {
         return arrayList.size();
     }
+
+    /**
+     * lets the user remove item from the list
+     * @param position
+     */
+    public void delete(int position){
+        Bill removed = arrayList.get(position);//saves the removed object
+        arrayList.remove(position);//removes the item
+        notifyItemRemoved(position);//makes the list update
+        showUndo(removed,position);
+    }
+
+    /**
+     * show option to undo delete
+     * @param removed the item that was removed
+     * @param position the position to insert
+     */
+    private void showUndo(final Bill removed, final int position){
+        View view = mActivity.findViewById(R.id.coordinatorLayout); //finds the fragment
+        Snackbar snackbar = Snackbar.make(view, "Do you want to undo the action",
+                Snackbar.LENGTH_LONG); //makes new snackbar
+        CoordinatorLayout.LayoutParams params = (CoordinatorLayout.LayoutParams)
+                snackbar.getView().getLayoutParams();
+        params.bottomMargin = 100; //make the snackbar above the buttom navigation view
+        snackbar.getView().setLayoutParams(params);
+        snackbar.setAction("UNDO", new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                    undoDelete(removed, position); //if undo pressed do the undoDeleelete action
+            }
+        }); //makes a action with text "UNDO" and  and attack click listener
+        snackbar.show(); //showes the snackbar
+    }
+
+    /**
+     * perform the actual undo
+     * @param removed the item that was removed
+     * @param position the position to insert
+     */
+    private void undoDelete(Bill removed, int position){
+        arrayList.add(position, removed); //adds new item
+        notifyItemInserted(position); //updates the list that new item is arrived
+    }
+
+}
+
+/**
+ * class that mangases row swiping
+ */
+class SwipeManager extends ItemTouchHelper.SimpleCallback{
+    private StableArrayAdapter mAdapter;
+
+    public SwipeManager(StableArrayAdapter mAdapter){
+        super(0,ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT);
+        this.mAdapter = mAdapter;
+    }
+
+    @Override
+    public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction){
+        switch (direction){
+            case ItemTouchHelper.RIGHT:
+                //item swiped to the right- delete the item
+                mAdapter.delete(viewHolder.getAdapterPosition());
+                break;
+            case ItemTouchHelper.LEFT:
+                //item swiped to the left- edit the item
+                break;
+        }
+    }
+
+    @Override
+    public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+        // used for up and down movements
+        return false;
+    }
+
 }
